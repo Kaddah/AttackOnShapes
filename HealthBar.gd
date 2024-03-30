fc5ed547dec05abef12b4626c74fbdcd666a3a04
Hühnlein, Moritz <moritz.huehnlein@stud.hs-coburg.de>
2024-03-30 15:42:09 +0100

    health bar

---
+++ HealthBar.gd
@@ -0,0 +1,10 @@
+extends ProgressBar
+
+@export var player: Player
+
+func _ready():
+	update()
+
+func update():
+	#value = player.currentHealth *100 / player.maxHealth
+	pass
--- player.gd
+++ player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
-
+class_name Player
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+signal healthChanged
 signal laser_shot(laser_scene, location, rotation)
+
+
 #signal laser_orientation(global_rotation)
 var laser_scene = preload("res://laser.tscn")
 
 @onready var muzzle = $Muzzle
+
+@onready var currentHealth: int = maxHealth
+@export var maxHealth = 5	
+@onready var healthChangedVar = false
+
+func _physics_process(delta):
+	handleCollisions()
 
 func _process(delta):
 	var forward = Input.is_action_pressed("forward")
@@ -33,5 +43,22 @@ func shoot():
 	laser_shot.emit(laser_scene, muzzle.global_position, rotation)
 	#laser_orientation.emit(global_rotation)
 
+func handleCollisions():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
 
-	
+
+func _on_hurt_box_area_entered(area):
+		currentHealth -= 1 
+		healthChangedVar = true
+		sendCollision()
+		if currentHealth < 0: 
+			currentHealth = maxHealth
+		print_debug(currentHealth)
+
+func sendCollision():
+
+	if healthChangedVar: 
+
+		healthChanged.emit(currentHealth)
