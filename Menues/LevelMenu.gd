c03b51c6b8531a7a883e510a918432713ab2a8a4
Katharina Bork <katha.bork75@gmail.com>
2024-06-20 13:29:47 +0200

    change scene

--- Menues/LevelMenu.gd
+++ Menues/LevelMenu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_exit_pressed():
-	get_tree().quit()# Replace with function body.
+	get_tree().change_scene_to_file("res://Menues/main_menu.tscn")# Replace with function body.
 
 
 func _on_level_1_pressed():
